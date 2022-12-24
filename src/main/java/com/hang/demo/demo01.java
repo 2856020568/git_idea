0c556c778f4e67c39f15bf8555d8e8ab179d4531
Tonly <tonly@abc.com>
2022-12-24 13:44:06 +0800

    test2

--- src/main/java/com/hang/demo/demo01.java
+++ src/main/java/com/hang/demo/demo01.java
@@ -3,5 +3,6 @@ package com.hang.demo;
 public class demo01 {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
